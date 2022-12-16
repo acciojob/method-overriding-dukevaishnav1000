becb496fc1e706c6871b9cd8704281cc75f86da0
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2022-12-16 22:20:21 +0530

    final submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        B objb=new B();
+        A obj2=new B();
+        System.out.println(obj2.meth());
+        System.out.println(objb.meth());
+    }
+}
+
+class A{
+
+    public static String meth(){
+        return "invoking method from class A";
+    }
+}
+
+class B extends A{
+
+    public static String meth() {
+        return "Method is overridden in Extended class B";
+    }
 }
